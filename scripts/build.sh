7a1183bee2d1d3d37b7db158c5b5eafd4f0747a7
reece-elder <reece.elder@qa.com>
2022-11-02 14:44:54 +0000

    changing workflow

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Shebang - Specifies this script should be run as a bash script
+echo "============================================="
+echo "Started!"
+ls -a
+pwd
+touch new_file1.txt
+mkdir newDirectory
+whoami
+echo "Finished!"
